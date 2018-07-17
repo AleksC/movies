d97b4c38d2574e3cdde783861d2b3229a4bee963
AleksC <aleksandar.cosic1@yahoo.com>
2018-07-17 20:45:14 +0200

    8. zadatak

---
+++ database/migrations/2018_07_17_183853_create_comments.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateComments extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->unsignedInteger('movie_id')->references('id')->on('movies')->onDelete('cascade');
+            $table->string('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
